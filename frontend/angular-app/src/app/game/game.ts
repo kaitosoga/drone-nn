46506da0228ff0180f0383bc9e70cc4099542fc2
Kaito0733 <riossart@gmail.com>
2026-02-03 00:48:57 +0100

    made sharp in canvas, added animations, tried images and fixed function import

--- frontend/angular-app/src/app/game/game.ts
+++ frontend/angular-app/src/app/game/game.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core'
-// import {Env} from './logic/Env'
+import { Env } from '../logic/Env';
 
 @Component({
   selector: 'app-game',
@@ -17,18 +17,65 @@ export class Game {
     return this.canvas.nativeElement.getContext('2d') || new CanvasRenderingContext2D(); // to avoid '?'
   }
 
+  skin = new Image();
+
   ngAfterViewInit() { // because constructor would attempt to draw before html starts to render
-    this.drawLine();
+    const env = new Env();
+
+    const canvas = this.canvas.nativeElement as HTMLCanvasElement;
+    const dpr = window.devicePixelRatio || 1
+    const rect = canvas.getBoundingClientRect();
+
+    canvas.width  = rect.width * dpr;
+    canvas.height = rect.height * dpr;
+    this.context.scale(dpr, dpr);
+
+    this.skin.src = 'media/camera-drone.png';
+    this.skin.onload = () => this.draw();
+    
+    this.draw();
   }
 
-  drawLine() {
+  idk() {
     this.context.beginPath();
     this.context.moveTo(50, 50);
     this.context.lineTo(100, 120);
     this.context.strokeStyle = "white"; // to assign check presence
     this.context?.stroke();
+  }
+
+  x = 50;
+  vx = 300;
+  lastTime = 0;
+
+  draw(time = 0) {
+    const dt = (time - this.lastTime) / 1000;
+    this.lastTime = time;
+
+    this.step(dt);
+    requestAnimationFrame(t => this.draw(t));
+  }
+
+  step(dt: number) {
+    this.x += this.vx * dt;
+    // this.env.update(this.x, ...)
+
+    const rightend = window.outerWidth * 0.75 - 50
+    if (this.x > rightend || this.x < 50) this.vx *= -1;
+
+    this.context.clearRect(0, 0, 800, 500);
+    this.context.fillStyle = 'white';
+    this.context.fillRect(this.x, 200, 40, 40);
+    if (!this.skin.complete) return;
+    this.context.drawImage(this.skin, this.x, 200, 40, 40);
+
 
   }
 
 
+
+
+
+
 }
+
